90befefb53bf4c335f0113b5a87a0f991f5f3a5b
Adamh1223 <adam@hussmedia.io>
2024-07-23 14:20:43 -0700

    created file/folder structure, added mongoose

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/social_network_db",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+module.exports = mongoose.connection;
