b41ecb05dd16c646df1b3fb6c7b462be5a2411a1
Adamh1223 <adam@hussmedia.io>
2024-07-23 20:51:05 -0700

    got mongodb to connect, added dotenv to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect(
-  process.env.MONGODB_URI,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+console.log("MONGODB_URI:", process.env.MONGODB_URI);
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
