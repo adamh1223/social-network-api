18cb4da04f0463636126e41ba82ae977b9867f2e
Adam Hussain <adam@hussmedia.io>
2024-07-23 19:15:43 -0700

    remove local host databse

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/social_network_db",
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
